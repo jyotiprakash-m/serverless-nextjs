ed9181c8cc140c1e43775d5e66d38cb813457b82
jyotiprakash-m <teachmeonline365@gmail.com>
2022-03-18 12:08:29 +0530

    do test

--- api/temp.js
+++ api/temp.js
@@ -1,27 +1,25 @@
-const express = require("express");
+const app = require('express')();
+const { v4 } = require('uuid');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-const PORT = process.env.PORT || 3001
 const S3 = require("aws-sdk/clients/s3");
 const fs = require("fs");
-require("dotenv").config();
 const mongoose = require("mongoose")
-// Monogdb connection
-const documentInfo = require("./models/documentInfoModel")
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(res => console.log("DB Connected")).catch(err => console.log(err))
-
+const documentInfo = require("../models/documentInfoModel")
+require("dotenv").config();
+const upload = multer({ dest: "uploads/" });
 
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const endpoint = process.env.ENDPOINT;
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(res => console.log("DB Connected")).catch(err => console.log(err))
 
 const s3 = new S3({
     accessKeyId,
@@ -33,29 +31,31 @@ const s3 = new S3({
     httpOptions: { timeout: 0 },
 });
 
-// defining the Express app
-const app = express();
+// Middleware 
 
-// adding Helmet to enhance your API's security
 app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: false,
     })
 );
-
-// enabling CORS for all requests
 app.use(cors());
-// app.use(upload.array());
-
-// adding morgan to log HTTP requests
 app.use(morgan("combined"));
 
+app.get('/api/documents', async (req, res) => {
+
+    const userDocuments = await documentInfo.find()
+    res.send(userDocuments);
+});
+app.post('/api/documents', async (req, res) => {
+
+    const userDocuments = await documentInfo.find()
+    res.send(userDocuments);
+});
+
 app.post(
-    "/storj/upload",
+    "/api/storj/upload",
     upload.single("doc"),
     async function (req, res, next) {
         try {
@@ -99,18 +99,40 @@ app.post(
     }
 );
 
-app.get("/storj/:key", async (req, res) => {
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+// app.get('/api/item/:slug', (req, res) => {
+//     const accessKeyId = process.env.ACCESS_KEY_ID;
+//     const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+//     const endpoint = process.env.ENDPOINT;
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug} - ${accessKeyId} - ${secretAccessKey} - ${endpoint} - `);
+// });
+app.get('/api/storj/:key', (req, res) => {
     const params = {
         Bucket: "demo-bucket",
         Key: req.params.key,
     };
     const url = s3.getSignedUrl("getObject", params);
-    console.log("url", url);
 
     res.send(url);
 });
 
-// starting the server
-// app.listen(PORT, () => {
-//   console.log(`listening on port ${PORT}`);
+
+// app.get('api/storj/:key', (req, res) => {
+//     const params = {
+//         Bucket: "demo-bucket",
+//         Key: req.params.key,
+//     };
+//     const url = s3.getSignedUrl("getObject", params);
+
+//     res.send(url);
 // });
+
+module.exports = app;
