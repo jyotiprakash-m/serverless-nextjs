b4abf5b610d17edcc580078f18957974a50a0cc2
jyotiprakash-m <teachmeonline365@gmail.com>
2022-03-18 12:23:57 +0530

    try

--- api/index.js
+++ api/index.js
@@ -1,27 +1,25 @@
-const express = require("express");
+const app = require('express')();
+const { v4 } = require('uuid');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
-const PORT = process.env.PORT || 3001
+// const multer = require("multer");
 const S3 = require("aws-sdk/clients/s3");
 const fs = require("fs");
-require("dotenv").config();
 const mongoose = require("mongoose")
-// Monogdb connection
-const documentInfo = require("./models/documentInfoModel")
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(res => console.log("DB Connected")).catch(err => console.log(err))
-
+const documentInfo = require("../models/documentInfoModel")
+require("dotenv").config();
+// const upload = multer({ dest: "uploads/" });
 
 const accessKeyId = process.env.ACCESS_KEY_ID;
 const secretAccessKey = process.env.SECRET_ACCESS_KEY;
 const endpoint = process.env.ENDPOINT;
 
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(res => console.log("DB Connected")).catch(err => console.log(err))
 
 const s3 = new S3({
     accessKeyId,
@@ -33,72 +31,89 @@ const s3 = new S3({
     httpOptions: { timeout: 0 },
 });
 
-// defining the Express app
-const app = express();
+// Middleware 
 
-// adding Helmet to enhance your API's security
 app.use(helmet());
-
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
 app.use(
     bodyParser.urlencoded({
         extended: false,
     })
 );
-
-// enabling CORS for all requests
 app.use(cors());
-// app.use(upload.array());
-
-// adding morgan to log HTTP requests
 app.use(morgan("combined"));
 
-app.post(
-    "api/storj/upload",
-    upload.single("doc"),
-    async function (req, res, next) {
-        try {
-            console.log(req.file);
-            console.log(req.body);
-            const userDocuments = await documentInfo.find({ email: req.body.email })
-            if (userDocuments.length < 5) {
-                const file = fs.readFileSync(req.file.path);
-                const params = {
-                    Bucket: "demo-bucket",
-                    Key: req.body.key,
-                    Body: file,
-                };
-                const result = await s3
-                    .upload(params, {
-                        partSize: 64 * 1024 * 1024,
-                    })
-                    .promise();
+app.get('/api/documents', async (req, res) => {
 
-                await fs.unlink(req.file.path, (err) => {
-                    if (err) throw err;
-                    console.log("successfully deleted");
-                });
-                const { Bucket, ETag, Key, Location, key } = result;
-                const documentData = await new documentInfo({
-                    Bucket, ETag, Key, Location, key, email: req.body.email, type: req.body.type
-                }).save()
-                const uploadedDocument = await documentInfo.findOne({ key: documentData.key })
-                return res.status(200).json({ uploadedDocument, message: "Document Uploaded Successfully" })
+    const userDocuments = await documentInfo.find()
+    res.send(userDocuments);
+});
+app.post('/api/storj/upload', async (req, res) => {
 
-                // res.send(result);
+    const userDocuments = await documentInfo.find({ email: req.body.email })
+    res.send(userDocuments);
+});
+
+// app.post(
+//     '/api/storj/upload',
+
+//     async  (req, res)=> {
+//         try {
+//             console.log(req.file);
+//             console.log(req.body);
+//             const userDocuments = await documentInfo.find({ email: req.body.email })
+//             if (userDocuments.length < 5) {
+//                 const file = fs.readFileSync(req.file.path);
+//                 const params = {
+//                     Bucket: "demo-bucket",
+//                     Key: req.body.key,
+//                     Body: file,
+//                 };
+//                 const result = await s3
+//                     .upload(params, {
+//                         partSize: 64 * 1024 * 1024,
+//                     })
+//                     .promise();
+
+//                 await fs.unlink(req.file.path, (err) => {
+//                     if (err) throw err;
+//                     console.log("successfully deleted");
+//                 });
+//                 const { Bucket, ETag, Key, Location, key } = result;
+//                 const documentData = await new documentInfo({
+//                     Bucket, ETag, Key, Location, key, email: req.body.email, type: req.body.type
+//                 }).save()
+//                 const uploadedDocument = await documentInfo.findOne({ key: documentData.key })
+//                 return res.status(200).json({ uploadedDocument, message: "Document Uploaded Successfully" })
+
+//                 // res.send(result);
 
 
-            } else {
-                return res.status(401).json({ error: "You exceeded the upload limit!" })
-            }
-        } catch (err) {
-            console.log("err", err);
-            res.send(err);
-        }
-    }
-);
+//             } else {
+//                 return res.status(401).json({ error: "You exceeded the upload limit!" })
+//             }
+//         } catch (err) {
+//             console.log("err", err);
+//             res.send(err);
+//         }
+//     }
+// );
 
+
+app.get('/api', (req, res) => {
+    const path = `/api/item/${v4()}`;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
+});
+
+// app.get('/api/item/:slug', (req, res) => {
+//     const accessKeyId = process.env.ACCESS_KEY_ID;
+//     const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+//     const endpoint = process.env.ENDPOINT;
+//     const { slug } = req.params;
+//     res.end(`Item: ${slug} - ${accessKeyId} - ${secretAccessKey} - ${endpoint} - `);
+// });
 app.get('/api/storj/:key', (req, res) => {
     const params = {
         Bucket: "demo-bucket",
@@ -109,9 +124,15 @@ app.get('/api/storj/:key', (req, res) => {
     res.send(url);
 });
 
-// starting the server
-// app.listen(PORT, () => {
-//   console.log(`listening on port ${PORT}`);
+
+// app.get('api/storj/:key', (req, res) => {
+//     const params = {
+//         Bucket: "demo-bucket",
+//         Key: req.params.key,
+//     };
+//     const url = s3.getSignedUrl("getObject", params);
+
+//     res.send(url);
 // });
 
 module.exports = app;
