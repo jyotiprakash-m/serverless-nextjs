b3389b676f88a59722172f1bca594bd77e0965d2
jyotiprakash-m <teachmeonline365@gmail.com>
2022-03-18 10:04:16 +0530

    test 2

--- api/index.js
+++ api/index.js
@@ -34,7 +34,7 @@ app.get('/api', (req, res) => {
 
 app.get('/api/item/:slug', (req, res) => {
     const { slug } = req.params;
-    res.end(`Item: ${slug}`);
+    res.end(`Item: ${slug} - ${accessKeyId} - ${secretAccessKey} - ${endpoint} - `);
 });
 
 app.get("api/storj/:key", async (req, res) => {
