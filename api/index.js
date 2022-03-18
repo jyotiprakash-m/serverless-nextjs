fcb4b17452a2a1b19f6dd90aaa315f2fcc3de0e8
jyotiprakash-m <teachmeonline365@gmail.com>
2022-03-18 10:07:59 +0530

    test 2

--- api/index.js
+++ api/index.js
@@ -1,29 +1,29 @@
 const app = require('express')();
 const { v4 } = require('uuid');
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const multer = require("multer");
-const S3 = require("aws-sdk/clients/s3");
-const fs = require("fs");
-const mongoose = require("mongoose")
+// const bodyParser = require("body-parser");
+// const cors = require("cors");
+// const helmet = require("helmet");
+// const morgan = require("morgan");
+// const multer = require("multer");
+// const S3 = require("aws-sdk/clients/s3");
+// const fs = require("fs");
+// const mongoose = require("mongoose")
 require("dotenv").config();
-const upload = multer({ dest: "uploads/" });
+// const upload = multer({ dest: "uploads/" });
 
-const accessKeyId = process.env.ACCESS_KEY_ID;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
-const endpoint = process.env.ENDPOINT;
+// const accessKeyId = process.env.ACCESS_KEY_ID;
+// const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+// const endpoint = process.env.ENDPOINT;
 
-const s3 = new S3({
-    accessKeyId,
-    secretAccessKey,
-    endpoint,
-    s3ForcePathStyle: true,
-    signatureVersion: "v4",
-    connectTimeout: 0,
-    httpOptions: { timeout: 0 },
-});
+// const s3 = new S3({
+//     accessKeyId,
+//     secretAccessKey,
+//     endpoint,
+//     s3ForcePathStyle: true,
+//     signatureVersion: "v4",
+//     connectTimeout: 0,
+//     httpOptions: { timeout: 0 },
+// });
 
 app.get('/api', (req, res) => {
     const path = `/api/item/${v4()}`;
@@ -33,19 +33,22 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/api/item/:slug', (req, res) => {
+    const accessKeyId = process.env.ACCESS_KEY_ID;
+    const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+    const endpoint = process.env.ENDPOINT;
     const { slug } = req.params;
     res.end(`Item: ${slug} - ${accessKeyId} - ${secretAccessKey} - ${endpoint} - `);
 });
 
-app.get("api/storj/:key", async (req, res) => {
-    const params = {
-        Bucket: "demo-bucket",
-        Key: req.params.key,
-    };
-    const url = s3.getSignedUrl("getObject", params);
-    console.log("url", url);
+// app.get("api/storj/:key", async (req, res) => {
+//     const params = {
+//         Bucket: "demo-bucket",
+//         Key: req.params.key,
+//     };
+//     const url = s3.getSignedUrl("getObject", params);
+//     console.log("url", url);
 
-    res.send(url);
-});
+//     res.send(url);
+// });
 
 module.exports = app;
